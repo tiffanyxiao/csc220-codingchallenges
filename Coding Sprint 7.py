f7b083823fe7a4cc38d7d79e1655156f1ad6c9f6
Tiffany Xiao <txiao8415@bths.edu>
2018-02-15 14:54:20 -0500

    Fixed coding sprint 7.

--- Coding Sprint 7.py
+++ Coding Sprint 7.py
@@ -8,26 +8,28 @@ identify whether or not there is a subset that could fill a
 Prime Pantry Box to exactly 100%
 
 Auther notes:
+
+To do:
+-sort it
 '''
+import multiprocessing as mp
+
 def primePantry(boxes, n_items, total):
     all_pantries = []
     for box in boxes:
         one_pantry = [box]
         weight = boxes.get(box)
-        print(weight)
         for otherbox in boxes:
-            if (weight + boxes.get(otherbox) < total):
+            if (weight + boxes.get(otherbox) <= total) and (otherbox not in one_pantry):
                 one_pantry.append(otherbox)
                 weight += boxes.get(otherbox)
-        all_pantries.append(one_pantry)
-
-
-    return all_pantries
-
-def 
+        if weight == 100:
+            return True
+    return False
 
 def main():
-    primeComplete = primePantry({"pepsi":55, "detergent":30, "chips":25, "cereal":15}, 4, 100)
-    print(primeComplete)
+    primeComplete = primePantry({"pepsi":55, "chips":25, "detergent":30, "cereal":15}, 4, 100)
+    if primeComplete:
+        print("True")
 
 main()
