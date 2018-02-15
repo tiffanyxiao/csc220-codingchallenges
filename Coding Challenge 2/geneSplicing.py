6f27285f66e20261e2fa678e07ce95ffefcbbdda
Tiffany Xiao <txiao8415@bths.edu>
2018-02-15 01:27:18 -0500

    Update code.

--- Coding Challenge 2/geneSplicing.py
+++ Coding Challenge 2/geneSplicing.py
@@ -21,15 +21,6 @@ To do:
 -remove test strings
 '''
 
-def string_union(string1, string2,substring):
-    '''make the shortest string  that contains string1 and string2 as subsequences'''
-    union_result = ""
-    if string1.endswith(substring):
-        union_result = string1+ string2.replace(substring,"")
-    else:
-        union_result = string2+ string1.replace(substring,"")
-    return union_result
-
 def get_overlap(string1, string2, len_string1, len_string2):
     '''Function to find the string that contains both strings.
 
@@ -40,75 +31,84 @@ def get_overlap(string1, string2, len_string1, len_string2):
     len_string2 - length of string2
 
     Return:
-    Returns the overlap string
+    Returns the shortest string that has both as substring
     '''
-    # string1 and string2 form a 2D array where string1 is the horizontal direction and
-    # string2 is the vertical direction
+    # automatic case if the strings are the same, return the string
+    if (string1 == string2):
+        return [string1]
 
-    # declare 2d array to store the values
-    arrayValues = [[None]*(len_string2+1) for i in range(len_string1+1)]
+    #string1 is associated with horizontal diraction
+    #string2 is associated with vertical direction
+    #everything initially filled in with 0's
+    matrix = [[0 for col in range(len_string1)] for row in range(len_string2)]
 
-    # build arrayValues from bottom up
-    for i in range(len_string1+1):
-        for j in range(len_string2+1):
-            if i == 0 or j == 0:
-                arrayValues[i][j] = 0
-            elif string1[i-1] == string2[j-1]:
-                arrayValues[i][j] = arrayValues[i-1][j-1]+1
-            else:
-                arrayValues[i][j] = max(arrayValues[i-1][j], arrayValues[i][j-1])
+    #fill in 1's with matches
+    for col in range (0,len_string1):
+        for row in range (0,len_string2):
+            if (string1[col] == string2[row]):
+                matrix[row][col] = 1
 
-    # if the max is 0, then print error (for now)
-    if arrayValues[len_string1][len_string2] == 0:
-        print("error")
-        return
-
-    # last element of 2d array (arrayValues[len_string1][len_string2] should have the length of the smallest substring)
-    # find the overlap position by using this number (find first instance of this number)
+    #for every cell in the matrix, add the value of the upper-reight cell to the current cell
+    #so the value of the cells will be the number of cosecutive match if there is any
+    overlap_len = 0
     overlap_position = []
-    for k in range(len_string1+1):
-        for l in range(len_string2+1):
-            if arrayValues[k][l] == arrayValues[i][j]:
-                overlap_position.append([k, l])
+    for col in range (0,len_string1):
+        for row in range (0,len_string2):
+            if matrix[row][col] == 1 and row!=0 and col!=0:
+                matrix[row][col] = matrix[row][col] + matrix[row-1][col-1]
+                if matrix[row][col]>overlap_len:
+                    overlap_len = matrix[row][col]
+                    overlap_position = [row,col]
 
-    # check that the first largest count is
+    large_share = 0
+    large_share_pos = []
 
-    # identify which string contains the substring
-    # if len_string1 == overlap_position[0] and len_string2 == overlap_position[1]:
-    #     # perform substring on longer string
-    # elif len_string1 == overlap_position[0]:
-    #     # perform substring on string2
-    # elif len_string2 == overlap_position[1]:
-    #     #perform substring on string1
+    for i in range(len(matrix)):
+        if (large_share < matrix[i][len_string1-1]):
+            current_pos = [i, len_string1-1]
+            keep_going = True
 
+            while (current_pos[0] != 0 and keep_going):
+                current_pos = [current_pos[0]-1, current_pos[1]-1]
+                if (matrix[current_pos[0]][ current_pos[1]] == 0):
+                    keep_going = False
 
-    #overlap_position = [[k,l] for k in range(len_string1+1) for l in range(len_string2+1) if arrayValues[k][l]==arrayValues[i][j] ]
-    print(arrayValues[i][j])
-    print(arrayValues)
-    print(overlap_position)
+            if (keep_going):
+                large_share = matrix[i][len_string1-1]
+                large_share_pos = [i,len_string1-1]
 
-    # #for every cell in the matrix, add the value of the upper-reight cell to the current cell
-    # #so the value of the cells will be the number of consecutive match if there is any
-    # overlap_len = 0
-    # overlap_position = []
-    # for col in range (0,len_string1):
-    #     for row in range (0,len_string2):
-    #         if matrix[row][col] == 1 and row!=0 and col!=0:
-    #             matrix[row][col] = matrix[row][col] + matrix[row-1][col-1]
-    #             if matrix[row][col]>overlap_len:
-    #                 overlap_len = matrix[row][col]
-    #                 overlap_position = [row,col]
-    #
-    # #construct the substring
-    # if (overlap_position):
-    #     overlap = string1[overlap_position[1]]
-    #     for l in range(1, overlap_len):
-    #         overlap = string1[overlap_position[1]-l]+overlap
-    #
-    # else:
-    #     overlap = ""
-    # return overlap
+    for i in range(len(matrix[len_string2-1])):
+        if(large_share < matrix[len_string2-1][i]):
+            current_pos = [len_string2-1,i]
+            keep_going = True
+            while(current_pos[1] != 0 and keep_going):
+                current_pos = [current_pos[0]-1, current_pos[1]-1]
+                if (matrix[current_pos[0]][ current_pos[1]] == 0):
+                    keep_going = False
+            if (keep_going):
+                large_share = matrix[len_string2-1][i]
+                large_share_pos = [len_string2-1, i]
 
+    for item in matrix:
+        print(item)
+
+    print("largest shared positon", large_share_pos)
+    print("largest sharted size", large_share)
+
+    #construct the substring
+    # if the largest shared size == 0
+    if (large_share == 0):
+        return [string1 + string2, string2 + string1]
+    # if string is substring of another
+    elif (large_share == len(string1)):
+        return [string2]
+    elif (large_share == len(string2)):
+        return [string1]
+    # if string is somewhere inbetween (either in front or in middle)
+    elif (large_share_pos[1]+1 == len(string2)):
+        return[string1+string2]
+    elif (large_share_pos[0]+1 == len(string1)):
+        return[string2+string1]
 
 def main():
     '''Function asks user to input two strings (two gene sequences), then calls other functions to get the overlap of the strings
@@ -118,8 +118,8 @@ def main():
     gene_list = ["A","C","T","G"]
 
     # intialize string1 and string2 (delete later)
-    string1 = "AATCG"
-    string2 = "GTTCG"
+    string1 = "AAA"
+    string2 = "BAAAB"
 
     # # check string inputs
     # string1 = input("Input the first sequence" + "\n").upper()
@@ -131,17 +131,15 @@ def main():
     # for l in string2:
     #     if (l not in gene_list):
     #         raise ValueError('String2 is not a gene')
-
     # create substring by generating overlap with strings
-    substring = get_overlap(string1,string2, len(string1), len(string2))
-    # get the union using substring result
-    union_result = string_union(string1, string2, substring)
+    union_result = get_overlap(string1,string2, len(string1), len(string2))
     # print in desired format
     print("**************************************")
     print("String 1 -", string1)
     print("String 2 -", string2)
     print("shortest string that has both as substring")
-    print(union_result, "(" + str(len(union_result)) + ")")
+    for union in union_result:
+        print(union, "(length " + str(len(union)) + ")")
     print("**************************************")
 
 main()
