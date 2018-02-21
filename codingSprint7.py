6dceba018d273eccb42f73314076c902b46da3c6
Tiffany Xiao <txiao8415@bths.edu>
2018-02-20 22:09:25 -0500

    Added reading from command line.

--- codingSprint7.py
+++ codingSprint7.py
@@ -24,6 +24,8 @@ max. recursion depth 1000
 Auther notes:
 
 '''
+import sys
+
 def primePantry(list_items, n_items, total) :
     ''' Function identifies whether or not there is a subset that could fill a
     Prime Pantry Box to exactly 100%
@@ -32,6 +34,8 @@ def primePantry(list_items, n_items, total) :
     n_items - number of items in list_items
     total - total/sum requested (100 in this challenge)
     '''
+    # create list of ints with list_items
+    list_items = [int(i) for i in range(len(list_items))]
 
     # return True if there's an element equal to total
     subset = [[True] * (total+1)] * (n_items+1)
@@ -53,6 +57,6 @@ def primePantry(list_items, n_items, total) :
             if (j >= list_items[i-1]) :
                 subset[i][j] = subset[i-1][j] or subset[i - 1][j-list_items[i-1]]
 
-    print("hello")
-
     return subset[n_items][total]
+
+primePantry(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
