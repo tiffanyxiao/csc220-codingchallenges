61e219d7109a2b5a647c28c2be02add4e7817016
Tiffany Xiao <txiao8415@bths.edu>
2018-02-21 21:33:29 -0500

    Pushed primePantry changes.

--- Coding Challenge 3/primePantry.py
+++ Coding Challenge 3/primePantry.py
@@ -19,24 +19,65 @@ Auther notes:
 
 To do:
 '''
-import multiprocessing as mp
+import sys
+import ast
 
-def primePantry(boxes, n_items, total):
-    all_pantries = []
-    for box in boxes:
-        one_pantry = [box]
-        weight = boxes.get(box)
-        for otherbox in boxes:
-            if (weight + boxes.get(otherbox) <= total) and (otherbox not in one_pantry):
-                one_pantry.append(otherbox)
-                weight += boxes.get(otherbox)
-        if weight == 100:
-            return True
-    return False
+def prime_pantry(dictItems, nItems, total) :
+    ''' Function identifies whether or not there is a subset that could fill a
+    Prime Pantry Box to exactly 100%
 
-def main():
-    primeComplete = primePantry({"pepsi":55, "chips":25, "detergent":30, "cereal":15}, 4, 100)
-    if primeComplete:
+    dictItems - list of all item weights (integer, positive valued)
+    nItems - number of items in dictItems
+    total - total/sum requested (100 in this challenge)
+    '''
+    # check for some cases in which we do not need to run the entire function
+    if total < 0 or total > sum(dictItems.values()):
+        print("False")
+        sys.exit()
+
+    # convert dictionary into list of lists for easier iterating through them
+    dictList = []
+    for key, value in dictItems.items():
+        temp = [key,value]
+        dictList.append(temp)
+
+    # create boolean array to fill the array with all the sub-totals from the subsets
+    # initialize all subsets to false
+    # note: sub-totals will be true when they have reached the total value
+    subset = []
+    for i in range(total+1):
+        sub_subset = [False,[]]
+        subset.append(sub_subset)
+
+    # set totals at index 0 to true
+    subset[0][0] = True
+    print(subset)
+
+    # address base case (there's only one element, and the element is equivalent to total)
+    if (nItems == 1 and dictList[0][1] == total) or total in dictList:
         print("True")
+        sys.exit()
 
-main()
+    # list all the sums between 0 and total, and try to find a subset for each sum until
+    # we find a sum for x (or reach the end of the list of items)
+    iterator = 0
+    # continue to fill while we have not reached end of subset and iterator is not at end
+    while not subset[total][0] and iterator < len( dictList ):
+        currentItem = dictList[iterator][1]
+        currentPos = total
+        # try to find a subset for the currentPos
+        while not subset[total][0] and currentPos >= currentItem:
+            if not subset[currentPos][0] and subset[currentPos - currentItem][0]:
+                subset[currentPos][1].append(currentPos)
+                subset[currentPos][1].append(currentPos-currentItem)
+                subset[currentPos][0] = True
+            currentPos -= 1
+        print(subset[currentPos+1][1])
+        iterator += 1
+        print(subset)
+
+    # print result
+    print(subset[total][0])
+    sys.exit()
+
+prime_pantry(ast.literal_eval(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
--- codingSprint7.py
+++ codingSprint7.py
@@ -8,23 +8,23 @@ identify whether or not there is a subset that could fill a
 Prime Pantry Box to exactly 100%
 
 How your function should run:
-prime_pantry(list, n_items, total)
+prime_pantry(list, nItems, total)
 '''
 import sys
 
-def prime_pantry(list_items, n_items, total) :
+def prime_pantry(listItems, nItems, total) :
     ''' Function identifies whether or not there is a subset that could fill a
     Prime Pantry Box to exactly 100%
 
-    list_items - list of all item weights (integer, positive valued)
-    n_items - number of items in list_items
+    listItems - list of all item weights (integer, positive valued)
+    nItems - number of items in listItems
     total - total/sum requested (100 in this challenge)
     '''
-    # create list of ints with list_items
-    list_items = [int(i) for i in list_items.strip('[]').split(',')]
+    # create list of ints with listItems
+    listItems = [int(i) for i in listItems.strip('[]').split(',')]
 
     # check for some cases in which we do not need to run the entire function
-    if total < 0 or total > sum(list_items):
+    if total < 0 or total > sum(listItems):
         print("False")
         sys.exit()
 
@@ -36,7 +36,7 @@ def prime_pantry(list_items, n_items, total) :
     subset[0] = True
 
     # address base case (there's only one element, and the element is equivalent to total)
-    if (n_items == 1 and list_items[0] == total) or total in list_items:
+    if (nItems == 1 and listItems[0] == total) or total in listItems:
         print("True")
         sys.exit()
 
@@ -44,15 +44,14 @@ def prime_pantry(list_items, n_items, total) :
     # we find a sum for x (or reach the end of the list of items)
     iterator = 0
     # continue to fill while we have not reached end of subset and iterator is not at end
-    while not subset[total] and iterator < len( list_items ):
-        current_item = list_items[iterator]
-        current_pos = total
-        # try to find a subset for the current_pos
-        while not subset[total] and current_pos >= current_item:
-            if not subset[current_pos] and subset[current_pos - current_item]:
-                subset[current_pos] = True
-            current_pos -= 1
-        print(subset)
+    while not subset[total] and iterator < len( listItems ):
+        currentItem = listItems[iterator]
+        currentPos = total
+        # try to find a subset for the currentPos
+        while not subset[total] and currentPos >= currentItem:
+            if not subset[currentPos] and subset[currentPos - currentItem]:
+                subset[currentPos] = True
+            currentPos -= 1
         iterator += 1
 
     # print result
